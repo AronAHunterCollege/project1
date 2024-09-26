2c32a41f513e4e43e66c20a9c88202e516c2d03f
AronAHunterCollege <92650310+AronAHunterCollege@users.noreply.github.com>
2024-09-25 21:53:48 -0400

    Delete Project1JSTitle.js

--- Project1JSTitle.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.fromTo(".title", { opacity: 0 }, { opacity: 1, duration: 3});
-  gsap.fromTo(".start", { opacity: 0 }, { opacity: 1, duration: 3, delay: 2});
-  gsap.fromTo(".info", { opacity: 0 }, { opacity: 1, duration: 3, delay: 3});
- });
